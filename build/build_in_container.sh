ef14cb0e47f6774d76e360c691f2a6a0f485a607
knall0 <32388161+knall0@users.noreply.github.com>
2017-12-04 16:16:01 +0100

    Add privileged-mode and proxy-envs to golang-Builder

--- build/build_in_container.sh
+++ build/build_in_container.sh
@@ -2,9 +2,12 @@
 
 binary="portainer-$1-$2"
 
+echo "Type in your proxysetting like http://username:Userpassword@proxy-server:Port"
+read proxyconnection
+
 mkdir -p dist
 
-docker run --rm -tv $(pwd)/api:/src -e BUILD_GOOS="$1" -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
+docker run --privileged --rm -tv $(pwd)/api:/src -e BUILD_GOOS="$1" -e HTTP_PROXY=$proxyconnection -e HTTPS_PROXY=$proxyconnection -e BUILD_GOARCH="$2" portainer/golang-builder:cross-platform /src/cmd/portainer
 
 mv "api/cmd/portainer/$binary" dist/
 #sha256sum "dist/$binary" > portainer-checksum.txt
